ec66d5e280af734d4057eafbe3d9c3a97d242e04
R4XGAMEZ <exeitachi824@gmail.com>
2026-04-19 18:36:22 +0530

    Create Task.java

---
+++ src/main/java/com/r4x/capcut_helper/model/Task.java
@@ -0,0 +1,13 @@
+package com.r4x.capcut_helper.model;
+
+public class Task {
+    public String action;
+    public int x;
+    public int y;
+
+    public Task(String action, int x, int y) {
+        this.action = action;
+        this.x = x;
+        this.y = y;
+    }
+}
