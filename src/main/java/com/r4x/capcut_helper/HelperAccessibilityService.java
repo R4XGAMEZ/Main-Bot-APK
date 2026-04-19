fb91ce5b7a94eeb797836e6558b315190429d5f3
R4XGAMEZ <exeitachi824@gmail.com>
2026-04-19 18:37:48 +0530

    Create HelperAccessibilityService.java

---
+++ src/main/java/com/r4x/capcut_helper/HelperAccessibilityService.java
@@ -0,0 +1,17 @@
+package com.r4x.capcut_helper;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class HelperAccessibilityService extends AccessibilityService {
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
